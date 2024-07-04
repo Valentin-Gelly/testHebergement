e750a264994c2b3b372617454e31315fb0d0f9e1
ValentinGELLY <gelly.valentin@gmail.com>
2024-07-04 13:52:53 +0200

    correction routing

--- src/app/prepaid-tariffs-telefonica/prepaid-tariffs-telefonica.component.ts
+++ src/app/prepaid-tariffs-telefonica/prepaid-tariffs-telefonica.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink,RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-prepaid-tariffs-telefonica',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive,RouterOutlet, CommonModule],
   templateUrl: './prepaid-tariffs-telefonica.component.html',
   styleUrl: './prepaid-tariffs-telefonica.component.css'
 })
--- src/app/printing-page/printing-page.component.ts
+++ src/app/printing-page/printing-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink,RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-printing-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive,RouterOutlet, CommonModule],
   templateUrl: './printing-page.component.html',
   styleUrl: './printing-page.component.css'
 })
