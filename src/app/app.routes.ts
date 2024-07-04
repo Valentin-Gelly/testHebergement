ca3e8150ebe3751ebff9dbf3980377022a1b7d13
ValentinGELLY <gelly.valentin@gmail.com>
2024-07-04 13:49:48 +0200

    add page choix des forfaits

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoadingImgComponent } from './loading-img/loading-img.component';
 import { PrintingPageComponent } from './printing-page/printing-page.component';
+import { PrepaidTariffsTelefonicaComponent } from './prepaid-tariffs-telefonica/prepaid-tariffs-telefonica.component';
 
 
 export const routes: Routes = [
   { path: 'homepage', component: HomepageComponent },
   {path: 'loading', component: LoadingImgComponent},
-  {path : 'printing', component: PrintingPageComponent}
+  {path : 'printing', component: PrintingPageComponent},
+  { path: 'prepaid-tariffs-telefonica', component: PrepaidTariffsTelefonicaComponent },
 ];
 
 @NgModule({
---
+++ src/app/prepaid-tariffs-telefonica/prepaid-tariffs-telefonica.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prepaid-tariffs-telefonica',
+  standalone: true,
+  imports: [],
+  templateUrl: './prepaid-tariffs-telefonica.component.html',
+  styleUrl: './prepaid-tariffs-telefonica.component.css'
+})
+export class PrepaidTariffsTelefonicaComponent {
+
+}
